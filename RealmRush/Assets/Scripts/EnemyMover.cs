b7efbac5d6ac486cc2a94c7b3e0a5537a735ee84
eksitus <uvay02@gmail.com>
2022-08-10 16:30:02 +0300

    .

--- RealmRush/Assets/Scripts/EnemyMover.cs
+++ RealmRush/Assets/Scripts/EnemyMover.cs
@@ -6,18 +6,25 @@ public class EnemyMover : MonoBehaviour
 {
 
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-
+    [SerializeField] float waitTime = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        PrintWayPointName();
+
+        StartCoroutine(FollowPath());
     }
 
-    void PrintWayPointName()
+    IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
         }
+
+        
+       
     }
+
+
 }
