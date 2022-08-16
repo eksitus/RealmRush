514d17e5d55639f30bb9a3763cf604a768ca6d2e
eksitus <uvay02@gmail.com>
2022-08-16 15:04:43 +0300

    .

--- RealmRush/Assets/PathFinding/GridManager.cs
+++ RealmRush/Assets/PathFinding/GridManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +5,10 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] Vector2Int gridSize;
-
     Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+    public Dictionary<Vector2Int, Node> Grid { get { return grid; } }
 
-    public Dictionary<Vector2Int,Node> Grid { get { return grid; } }
-
-    private void Awake()
+    void Awake()
     {
         CreateGrid();
     }
@@ -22,10 +19,37 @@ public class GridManager : MonoBehaviour
         {
             return grid[coordinates];
         }
+
         return null;
     }
 
-    private void CreateGrid()
+    public void BlockNode(Vector2Int coordinates)
+    {
+        if (grid.ContainsKey(coordinates))
+        {
+            grid[coordinates].isWalkable = false;
+        }
+    }
+
+    public Vector2Int GetCoordinatesFromPosition(Vector3 position)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        coordinates.x = Mathf.RoundToInt(position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        return coordinates;
+    }
+
+    public Vector3 GetPositionFromCoordinates(Vector2Int coordinates)
+    {
+        Vector3 position = new Vector3();
+        position.x = coordinates.x * UnityEditor.EditorSnapSettings.move.x;
+        position.z = coordinates.y * UnityEditor.EditorSnapSettings.move.z;
+
+        return position;
+    }
+
+    void CreateGrid()
     {
         for (int x = 0; x < gridSize.x; x++)
         {
--- RealmRush/Assets/Tiles/Waypoint.cs
+++ RealmRush/Assets/Tiles/Waypoint.cs
@@ -16,6 +16,28 @@ public class Waypoint : MonoBehaviour
         }
     }
 
+    GridManager gridManager;
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+    }
+
+    void Start()
+    {
+        if (gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if (!isPlaceable)
+            {
+                gridManager.BlockNode(coordinates);
+            }
+        }
+    }
+
+
     private void OnMouseDown()
     {
         if (isPlaceable)
